1d396997d0ff1552827a6000700d42e166f800a0
ashik <ashikes@gmail.com>
2018-04-13 18:21:13 +0530

    Adding more cards.

--- apps/demo-app/src/app/app.module.ts
+++ apps/demo-app/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
 import { HttpModule } from '@angular/http';
-import { HomeComponent } from './home';
 import { KanbanComponent } from './kanban';
 import { routing } from './app.routes';
 import { MatIconModule, MatToolbarModule } from '@angular/material';
@@ -11,7 +10,7 @@ import { SortableModule } from '@angular-sortable/sortable';
 
 @NgModule({
   imports: [BrowserModule, NxModule.forRoot(), HttpModule, routing, MatToolbarModule, MatIconModule, SortableModule],
-  declarations: [AppComponent, HomeComponent, KanbanComponent],
+  declarations: [AppComponent, KanbanComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- apps/demo-app/src/app/app.routes.ts
+++ apps/demo-app/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home';
+import { KanbanComponent } from "./kanban";
 
 export const routes: Routes = [
   {
     path: '', redirectTo: 'home', pathMatch: 'full'
   },
   {
-    path: 'home', component: HomeComponent
+    path: 'home', component: KanbanComponent
   }
 ];
 
